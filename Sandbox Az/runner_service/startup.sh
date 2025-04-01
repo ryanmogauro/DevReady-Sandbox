a030b39b540e6d7f1563f8a00448e9f8da5a0282
Ryan Mogauro <89362786+ryanmogauro@users.noreply.github.com>
2025-03-31 21:24:43 -0400

    increase thread count with ulimit

--- Sandbox Az/runner_service/startup.sh
+++ Sandbox Az/runner_service/startup.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 set -e
 
-# Install dependencies
-pip install --no-cache-dir -r requirements.txt
+#Set ulimit to enable more threads
+ulimit -u 4096
+
+ulimit -u
 
 # Start the app
-gunicorn --bind=0.0.0.0:${PORT:-8000} \
-         --workers=2 \
-         --threads=4 \
-         --timeout=300 \
-         --access-logfile=- \
-         --error-logfile=- \
-         --log-level=info \
-         run_server:app
+echo "Starting Gunicorn server..."
+exec gunicorn --bind=0.0.0.0:${PORT:-8000} \
+     --workers=2 \
+     --threads=4 \
+     --timeout=300 \
+     --access-logfile=- \
+     --error-logfile=- \
+     --log-level=info \
+     run_server:app
