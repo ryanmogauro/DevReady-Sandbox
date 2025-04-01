8d2ba31296be9d9db9b2c97e01cb598880564dd0
Ryan Mogauro <89362786+ryanmogauro@users.noreply.github.com>
2025-04-01 00:39:56 -0400

    Update code_executor.py

--- Sandbox Az/runner_service/code_executor.py
+++ Sandbox Az/runner_service/code_executor.py
@@ -35,8 +35,14 @@ LANGUAGE_COMMANDS = {
     'python': lambda filename: ["python3", filename],
     'javascript': lambda filename: ["node", filename],
     'js': lambda filename: ["node", filename],
-    'java': lambda filename: ["java", "-Xmx64m", "-Xms16m", "-XX:ConcGCThreads=2", "-XX:ParallelGCThreads=2", Path(filename).stem],
-    'go': lambda filename: ["go", "run", "-gcflags", "-N -l", filename],
+    # Limit Java threads
+    'java': lambda filename: ["java", "-Xmx64m", "-Xms16m", 
+                             "-XX:ConcGCThreads=2", 
+                             "-XX:ParallelGCThreads=2",
+                             "-Djava.util.concurrent.ForkJoinPool.common.parallelism=2",
+                             Path(filename).stem],
+    # Set GOMAXPROCS for Go
+    'go': lambda filename: ["sh", "-c", f"GOMAXPROCS=2 go run -gcflags='-N -l' {filename}"],
     'typescript': lambda filename: ["sh", "-c", f"tsc {filename} && node {Path(filename).with_suffix('.js')}"],
     'ts': lambda filename: ["sh", "-c", f"tsc {filename} && node {Path(filename).with_suffix('.js')}"]
 }
@@ -206,7 +212,7 @@ def execute_code(language: str, code_snippet: str, timeout: int = DEFAULT_TIMEOU
         def limit_resources():
             # Set max proc count
             #High count for Go & Java threads
-            resource.setrlimit(resource.RLIMIT_NPROC, (1000, 1000))
+            resource.setrlimit(resource.RLIMIT_NPROC, (100, 100))
             
             # Set CPU time limit, in secs
             resource.setrlimit(resource.RLIMIT_CPU, (timeout, timeout))
